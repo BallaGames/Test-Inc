e32ac91c8600acc9822a92624ca506fdbf4f1df5
LunarHackett003 <61294652+LunarHackett003@users.noreply.github.com>
2025-12-11 15:53:17 +0000

    player name refresh

--- Assets/Prefabs/UI/PlayerNameDisplay.cs
+++ Assets/Prefabs/UI/PlayerNameDisplay.cs
@@ -7,16 +7,33 @@ using UnityEngine;
 public class PlayerNameDisplay : BallaNetScript
 {
     public TMP_Text tmp;
-
+    string pname;
+    float t;
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
+        CheckName();
+    }
+    protected override void Timestep()
+    {
+        if(t <= 30)
+        {
+            t += Time.fixedDeltaTime;
+        }
+        else
+        {
+            CheckName();
+        }
+        
+    }
+    void CheckName()
+    {
         if (tmp)
         {
-            tmp.text = SteamFriends.GetFriendPersonaName(new CSteamID(PlayerController.clientSteamIDs[OwnerClientId]));
+            pname = SteamFriends.GetFriendPersonaName(new CSteamID(PlayerController.clientSteamIDs[OwnerClientId]));
+            tmp.text = pname;
         }
     }
-
     protected override void AfterFrame()
     {
         transform.LookAt(Camera.main.transform, Camera.main.transform.up);
